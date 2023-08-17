93e43066042b67819f485fdaeff6ac83257e276a
jdbpt <jbrown5.filn@gmail.com>
2023-08-17 16:55:19 -0400

    fixes to wk13day3 info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Fruit = require('./models/Fruit.js');
 const express = require('express');
 const fruits = require('./models/fruits');
 const veggies = require('./models/veggies');
-
+//Fruit.insertMany(fruits); //inserted alot of these multiple times, so this worked
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -39,21 +39,23 @@ app.use(express.urlencoded({extended:false}));
 
 //routes
 app.get('/', (req, res)=>{
-    res.send("<h1>Explore Veggies and See/Add fruits</h1>")
-})
-app.get('/fruits/', (req, res) => {
+    res.send("<h1>Explore Veggies and See/Add fruits</h1><nav><a href='/fruits'>Main Fruits Page</a></nav>")
+});
+app.get('/fruits', async (req, res) => {
     // res.render('Index', {
     //     fruits: fruits
     // });
     // res.render('fruits/Index');
-    Fruit.find({}, (error, allFruits)=>{
-        res.render('fruits/Index', {
+    // console.log(Fruit);
+    await Fruit.find({}).then((allFruits)=>{
+        res.render('Index', {
             fruits: allFruits
         });
+        // console.log(allFruits);
     });
 });
 
-app.post('/fruits', (req, res)=>{
+app.post('/fruits', async (req, res)=>{
     if (req.body.readyToEat === 'on') {//if checked the body is on
         req.body.readyToEat = true;
         
@@ -62,24 +64,26 @@ app.post('/fruits', (req, res)=>{
     }
     // fruits.push(req.body);
     // res.redirect('/fruits');
-    Fruit.create(req.body, (error, createdFruit)=>{
+
+    await Fruit.create(req.body).then( (error, createdFruit)=>{
         res.redirect('/fruits');
     });
+    console.log(fruits);//error checking
 });
 
 
 //adding a new fruit
-app.get('/fruits/new', (req, res) => {
+app.get('/fruits/new', async (req, res) => {
     res.render('New');
 });
 
 //show route (show routes use a get request)
-app.get('/fruits/:id', (req, res) => {
+app.get('/fruits/:id', async (req, res) => {//added async
 //     res.render('Show', {//second param must be an object
 //         fruit: fruits[req.params.indexOfFruitsArray]//corresponds to variable in Show file 
 // })
-Fruit.findById(req.params.id, (err, foundFruit)=>{
-    res.render('fruits/Show', {
+await Fruit.findById(req.params.id).then((foundFruit)=>{//deleted err
+    res.render('Show', {
         fruit: foundFruit
     });
 });
